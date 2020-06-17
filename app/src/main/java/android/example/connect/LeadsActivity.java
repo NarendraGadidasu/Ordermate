08f154302638c8e87a60ee38b395be0e6a732118
gadi <ngadidasu@gmail.com>
2020-06-17 11:02:43 -0400

    Added leads activity

---
+++ app/src/main/java/android/example/connect/LeadsActivity.java
@@ -0,0 +1,14 @@
+package android.example.connect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeadsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leads);
+    }
+}
