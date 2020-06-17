52caa1b3f766117e301bc6caed4522d0ca652db7
gadi <ngadidasu@gmail.com>
2020-06-17 12:05:17 -0400

    Added lead class, lead list item, list view, round corner buttons

---
+++ app/src/main/java/android/example/connect/Lead.java
@@ -0,0 +1,32 @@
+package android.example.connect;
+
+public class Lead {
+
+    private String name;
+    private double rating;
+    private double distance;
+    private double amount;
+
+    public Lead(String nme, double rtng, double dist, double amt){
+        name = nme;
+        rating = rtng;
+        distance = dist;
+        amount = amt;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
