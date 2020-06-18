19e8097723bda4626bc12c6bbb4163f877a5e96a
gadi <ngadidasu@gmail.com>
2020-06-18 05:08:57 -0400

    hidden action bar in login page

--- app/src/main/java/android/example/connect/MyloginActivity.java
+++ app/src/main/java/android/example/connect/MyloginActivity.java
@@ -16,11 +16,17 @@ public class MyloginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /**hide status bar*/
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        /**hide action bar*/
+        getSupportActionBar().hide();
+
+        /**set the content on load*/
         setContentView(R.layout.activity_mylogin);
 
+        /**Define what should happen on clicking the button login*/
         Button login = (Button) findViewById(R.id.login);
 
         login.setOnClickListener(new View.OnClickListener() {
