24979d000a3198b1b7e345283ab6c002156ee017
gadi <ngadidasu@gmail.com>
2020-06-18 14:26:38 -0400

    included contents of request class

--- app/src/main/java/android/example/connect/Request.java
+++ app/src/main/java/android/example/connect/Request.java
@@ -1,4 +1,46 @@
 package android.example.connect;
 
 public class Request {
+
+    private int userId;
+    private long requestId;
+    private String location;
+    private String business;
+    private double yourAmount;
+    private double expectedAmount;
+    private double maxDist;
+    private double maxWait;
+
+    public Request(int userId, long requestId, String loc, String bus, double yAmt, double eAmt, double mDist, double mWait){
+        location = loc;
+        business = bus;
+        yourAmount = yAmt;
+        expectedAmount = eAmt;
+        maxDist = mDist;
+        maxWait = mWait;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public double getYourAmount() {
+        return yourAmount;
+    }
+
+    public double getExpectedAmount() {
+        return expectedAmount;
+    }
+
+    public double getMaxDist() {
+        return maxDist;
+    }
+
+    public double getMaxWait() {
+        return maxWait;
+    }
 }
